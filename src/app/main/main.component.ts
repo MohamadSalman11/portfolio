49efc838fe0d6bdf627cbeec6b6f86a09d10a013
MohamadSalman11 <hamudesalman69@gmail.com>
2025-07-01 20:45:06 +0200

    add why me section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import {MeComponent} from "./me/me.component"
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeroComponent, NavbarComponent],
+  imports: [HeroComponent, NavbarComponent, MeComponent],
   templateUrl: './main.component.html',
 })
 export class MainComponent {}
