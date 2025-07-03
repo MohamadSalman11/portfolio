871659ea665d0eb7bde027455f82e379a702a7d5
MohamadSalman11 <hamudesalman69@gmail.com>
2025-07-03 15:56:07 +0200

    add projects section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from './hero/hero.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MeComponent } from './me/me.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeroComponent, NavbarComponent, MeComponent, SkillsComponent],
+  imports: [HeroComponent, NavbarComponent, MeComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './main.component.html',
 })
 export class MainComponent {}
