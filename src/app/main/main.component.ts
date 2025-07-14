cea0cd5f73b720d42f06618a647f558450657883
MohamadSalman11 <hamudesalman69@gmail.com>
2025-07-14 02:30:36 +0200

    add my courses section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,21 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { MeComponent } from './me/me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeroComponent, NavbarComponent, MeComponent, SkillsComponent, ProjectsComponent],
+  imports: [
+    HeroComponent,
+    NavbarComponent,
+    MeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    CoursesComponent,
+    ContactComponent,
+  ],
   templateUrl: './main.component.html',
 })
 export class MainComponent {}
